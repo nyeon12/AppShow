64ad46a5f58b4259a675df7bd5e95ea91b72f840
nyeon12 <30344455+nyeon12@users.noreply.github.com>
2017-10-12 00:41:09 +0900

    믱

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+    public float speed; 
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (transform.position.y < -29.1f)
+        {
+            transform.position = new Vector2(0, 0);
+        }
+    }
+}
---
+++ Assets/Script/Obstacle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject[] CreatFireBall = new GameObject[3];
+    public float randomX;
+    public float randomSeconds;
+    public GameObject GameOverBg;
+
+    private void Start()
+    {
+
+        StartCoroutine(ObjectRandomGenerator());
+    }
+    
+    IEnumerator ObjectRandomGenerator()
+    {
+       
+        while (true)
+        {
+            randomX = Random.Range(-1.7f, 2f);
+            randomSeconds = Random.Range(1f,2f);
+
+            Instantiate(CreatFireBall[Random.Range(0, 3)] ,new Vector2(randomX, 7f) ,Quaternion.identity);
+            yield return new WaitForSeconds(randomSeconds);
+        }
+    }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.tag == "Rocket")
+        {
+            GameOverBg.SetActive(true);
+        }
+    }
+
+}
